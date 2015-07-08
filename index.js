c22948da9cc5c0c2e4f0dfd7059488d34a21586b
Siedrix <Siedrix@gmail.com>
2015-07-08 17:51:51 -0500

    Added support for this.locals

--- index.js
+++ index.js
@@ -104,6 +104,9 @@ function renderer(settings) {
 
     // merge settings.locals
     mixin(opts, locals);
+    if(this.locals){
+      mixin(opts, this.locals);
+    }
 
     // merge options
     mixin(opts, options || {});
